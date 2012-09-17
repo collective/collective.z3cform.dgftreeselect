52b763d193afe4116019959c4bcb8624e4b5f9cf
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-09-18 01:34:38 +0300

    Helan går

--- README.txt
+++ README.txt
@@ -9,3 +9,14 @@ Nested choice trees with z3c.form and data grid widget.
 
 * Input a multi-selection choice tree as dictionaries
 
+Usage
+=====
+
+* Possible value map is created in a nested dictionary structure which nicely serializes
+  to JSON
+
+* Applies to zope.schema-TextLine field. Note that we cannot use zope.schema.Choice, becaues
+  of tree-like data would be very difficult to validate per cell
+
+* All fields in the nested tree structure must be non-required
+
--- src/collective/z3cform/dgftreeselect/testform.py
+++ src/collective/z3cform/dgftreeselect/testform.py
@@ -4,30 +4,103 @@
 
 """
 
+import json
+
 from zope import schema
 from zope import interface
+from zope.component import getMultiAdapter
 from Products.CMFCore.interfaces import ISiteRoot
-
-from z3c.form import field
-
-from plone.directives import form
-
 
 from five import grok
 
+from plone.directives import form
+
 from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
+from .widget import DGFTreeSelectFieldWidget
+
+
+SAMPLE_DATA = [
+    {
+        "id": "a",
+        "name": "A",
+        "children": [
+            {
+                "id": "aa",
+                "name": "AA",
+                "children": [
+                    {
+                        "id ": "aaa",
+                        "name ": "AAA",
+                    }
+                ]
+            },
+
+            {
+                "id": "ab",
+                "name": "AB",
+                "children": [
+                    {
+                        "id": "aba",
+                        "name": "ABA",
+                    },
+
+                    {
+                        "id": "abb",
+                        "name": "ABB",
+                    }
+
+                ]
+            },
+
+        ]
+    },
+
+    {
+        "id": "b",
+        "name": "B",
+        "children": [
+            {
+                "id": "ba",
+                "name": "BA",
+                "children": [
+                    {
+                        "id ": "baa",
+                        "name ": "BAA",
+                    }
+                ]
+            },
+
+            {
+                "id": "bb",
+                "name": "BB",
+                "children": [
+                    {
+                        "id": "bba",
+                        "name": "BBA",
+                    },
+
+                    {
+                        "id": "bbb",
+                        "name": "BBB",
+                    }
+
+                ]
+            },
+
+        ]
+    },
+]
 
 
 class ITableRowSchema(interface.Interface):
-    one = schema.TextLine(title=u"Level 1")
-    two = schema.TextLine(title=u"Level 2")
-    three = schema.TextLine(title=u"Level 3")
+    one = schema.TextLine(title=u"Level 1", required=False)
+    two = schema.TextLine(title=u"Level 2", required=False)
+    three = schema.TextLine(title=u"Level 3", required=False)
 
 
 class IFormSchema(form.Schema):
 
-    four = schema.TextLine(title=u"Four")
-
+    form.widget(table=DataGridFieldFactory)
     table = schema.List(title=u"Nested selection tree test",
         value_type=DictRow(title=u"tablerow", schema=ITableRowSchema))
 
@@ -42,7 +115,53 @@ class EditForm(form.SchemaForm):
 
     label = u"Tree selection demo and manual testing"
 
-    def updateFields(self):
-        form.SchemaForm.updateFields(self)
-        self.fields['table'].widgetFactory = DataGridFieldFactory
-        import ipdb ; ipdb.set_trace()
+    def datagridInitialise(self, subform, field):
+        """ Callback to customize the datagridfield
+
+        :param field: DataGridField instance
+
+        :param subform: DataGridFieldObjectSubForm instance
+        """
+
+        # Turn all fields in the table to use custom election widgets
+        for field in subform.fields.values():
+            field.widgetFactory = DGFTreeSelectFieldWidget
+
+        # CReate
+
+    def datagridUpdateWidgets(self, subform, widgets, widget):
+        """
+        """
+
+        # Set tree-mapping master-slace relationships between
+        # widgets in the row
+
+        widgets["one"].master = None
+        widgets["one"].slave = "two"
+
+        widgets["two"].master = "one"
+        widgets["two"].slave = "three"
+
+        widgets["three"].master = "two"
+        widgets["three"].slave = None
+
+        context = self.context.aq_inner
+        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+
+        # AJAX will load tree source data from this URL
+        sourceURL = portal_state.portal_url() + "/dgftreeselect-test-data"
+
+        for widget in widgets.values():
+            widget.sourceURL = sourceURL
+
+
+class DataSource(grok.CodeView):
+    """
+    Generate JSON array needed to populate the fields
+    """
+    grok.context(ISiteRoot)
+    grok.name("dgftreeselect-test-data")
+
+    def render(self):
+        self.request.response.setHeader("Content-type", "application/json")
+        return json.dumps(SAMPLE_DATA)
